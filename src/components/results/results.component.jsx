b2f5b0b3b2d600d25b7df2ad33d92aba7740dd01
Abhinav Sancheti <001sancheti@gmail.com>
2021-09-01 16:07:19 +0530

    quiz app completed

--- src/components/results/results.component.jsx
+++ src/components/results/results.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import './results.styles.scss'
+import './results.styles.scss';
 
 const Results = ({ questionText, correctAnswer, index, userAnswers }) => {
     return (
-        <div className="result-box">
-            <div>Question {index + 1}: {questionText}</div>
+        <div
+            className={`${
+                correctAnswer === userAnswers[index] ? 'correct' : 'incorrect'
+            } result-box`}
+        >
+            <div>
+                Question {index + 1}: {questionText}
+            </div>
             <div>Correct Answer: {correctAnswer}</div>
             <div>Your Answer: {userAnswers[index]}</div>
         </div>
